68ddc0f45bdb6bd6c0af35162a8499296b1eb0b5
Steven Almeroth <steven.almeroth@gmail.com>
2024-02-03 00:00:06 -0500

    cleanups

---
+++ README.md
@@ -0,0 +1,4 @@
+## Look ma! No template engine, just JavaScript.
+![main.ts](https://pmx.services/i/main.ts2.jpg)
+![main.html](https://pmx.services/i/main.html.jpg)
+![Browser](https://pmx.services/i/browser.jpg)
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import '@/style.css'
 import viteLogo from '/vite.svg'
 import typescriptLogo from '@/typescript.svg'
+// Done importing assets
+
 import { setupCounter } from '@/counter.ts'
+// Done importing modules
+
 import template from '@/main.html?raw'
+// Done importing template
+// Done importing
 
 const params = ["viteLogo", "typescriptLogo", `return \`${template}\``]
 const templateFunction = new Function(...params)
+// Done creating template function (compile)
+
 const app = document.querySelector<HTMLDivElement>('#app')!
+// Done selecting app element (dom)
+
 app.innerHTML = templateFunction(viteLogo, typescriptLogo)
+// Done rendering template (hydrate)
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Counter listening
