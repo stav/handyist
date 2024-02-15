a4877d3e5b6dd82a2f1e78a6fc005d8b77139956
Steven Almeroth <steven.almeroth@gmail.com>
2024-02-14 20:09:10 -0500

    sheeps
    WUP1
    
    WUP2
    
    WUP3
    
    WUP4

---
+++ src/_data/data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reviews: [
+    {name: 'Mehrooq M.', rating: 5, date: 'Nov 19, 2023', text: "Steve replaced our pedestal sink and old mirror with a new vanity/sink/faucet and new framed mirror. He was very responsive, punctual, honest and professional. Even though the job was more complex and took longer than expected due to our pipe layout, he stayed several extra hours until the job was done and kept the original quoted price for the project. We'd definitely use him again!"},
+    {name: 'Michael L.', rating: 5, date: 'Oct 10, 2023', text: "Steve was very meticulous and detail oriented. Did an excellent job and was very reasonable in his price. I would highly recommend him."},
+    {name: 'Tim P.'    , rating: 5, date: 'Nov 4, 2023' , text: "Gained a valuable resource to help with future projects. Thanks Steven !!"},
+  ],
+};
